e7f002be3730455067a7f99f5a2c2b2abe453e86
Code Nation 021 <codenation021@codes-mbp.lan>
2022-05-26 10:23:05 +0100

    added user input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import UserInput from './Components/UserInput';
 // Giving App the type of a Function Component
 const App: React.FC = () => {
   return (
     <div className='App'>
       <h1 className='heading'>pomodoro tasks</h1>
+      <UserInput />
     </div>
   )
 }
---
+++ src/Components/UserInput.tsx
@@ -0,0 +1,9 @@
+const UserInput = () => {
+    return (
+        <form>
+            <input type="input" placeholder="enter task" />
+            <button>go</button>
+        </form>
+    )
+}
+export default UserInput
